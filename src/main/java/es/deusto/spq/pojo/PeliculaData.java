14ba50911d65f106f77574772acdc73aee98e567
Iker-Marcelo <ikermarcelo@opendeusto.es>
2023-03-29 17:45:21 +0200

    Corregir clase PeliculaData

--- src/main/java/es/deusto/spq/pojo/PeliculaData.java
+++ src/main/java/es/deusto/spq/pojo/PeliculaData.java
@@ -1,16 +1,78 @@
 package es.deusto.spq.pojo;
 
+import es.deusto.spq.server.jdo.Genero;
+
 public class PeliculaData {
-	private String pelicula;
+	
+	private String codigo;
+	private String titulo;
+	private Genero genero;
+	private int minutos;
+	private int valoracion;
+	
 	
 	public PeliculaData() {
 		
 	}
-	public String getPelicula() {
-		return this.pelicula;
+
+
+	public String getCodigo() {
+		return codigo;
 	}
-	public void setPelicula(String pelicula) {
-		this.pelicula = pelicula;
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+
+	public int getValoracion() {
+		return valoracion;
+	}
+
+
+	public void setValoracion(int valoracion) {
+		this.valoracion = valoracion;
+	}
+
+
+	@Override
+	public String toString() {
+		return "PeliculaData [codigo=" + codigo + ", titulo=" + titulo + ", genero=" + genero + ", minutos=" + minutos
+				+ ", valoracion=" + valoracion + "]";
+	}
+	
+	
+
 	
 }
