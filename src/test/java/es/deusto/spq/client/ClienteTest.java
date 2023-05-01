64cba71ff69a10842deae7e90d56116bfee2ce4c
Iker-Marcelo <ikermarcelo@opendeusto.es>
2023-05-01 18:46:52 +0200

    Cliente Test
    He hecho un test del cliente

---
+++ src/test/java/es/deusto/spq/client/ClienteTest.java
@@ -0,0 +1,69 @@
+package es.deusto.spq.client;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.pojo.AdminData;
+import es.deusto.spq.pojo.UserData;
+import es.deusto.spq.server.jdo.User;
+
+
+
+public class ClienteTest {
+
+    private Client client;
+    private WebTarget webTarget;
+
+    @Before
+    public void setUp() {
+        String hostname = "localhost";
+        String port = "8080";
+        client = ClientBuilder.newClient();
+        webTarget = client.target(String.format("http://%s:%s/rest/resource", hostname, port));
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(client);
+        assertNotNull(webTarget);
+    }
+    
+//    @Test
+//    public void testRegisterAdmin() {
+//        // Given
+//        String login = "admin";
+//        String password = "password";
+//        WebTarget registerAdminWebTarget = webTarget.path("adminRegister");
+//        Invocation.Builder invocationBuilder = registerAdminWebTarget.request(MediaType.APPLICATION_JSON);
+//
+//        AdminData adminData = new AdminData();
+//        adminData.setLogin(login);
+//        adminData.setPassword(password);
+//
+//        // When
+//        Response response = invocationBuilder.post(Entity.entity(adminData, MediaType.APPLICATION_JSON));
+//
+//        // Then
+//        assertEquals(Status.OK.getStatusCode(), response.getStatus());
+//    }
+    
+    
+    
+}
+
+
