4ba5dc54613d347752668b6bf0e30f91e303a38d
Joseba-Loidi <91148798+Joseba-Loidi@users.noreply.github.com>
2023-05-19 21:39:13 +0200

    UltimaPrueba

--- src/test/java/es/deusto/spq/server/server/ResourceIntegrationTest.java
+++ src/test/java/es/deusto/spq/server/server/ResourceIntegrationTest.java
@@ -87,32 +87,32 @@ private static final PersistenceManagerFactory pmf = JDOHelper.getPersistenceMan
             pm.close();
         }
     }
-    @Test
-    public void testRegisterUser() {
-        User user = new User();
-        user.setLogin(UUID.randomUUID().toString());
-        user.setPassword("Sancet");
-        user.setCorreo("Sancet@gmail.com");
-
-        Response response = target.path("register")
-            .request(MediaType.APPLICATION_JSON)
-            .post(Entity.entity(user, MediaType.APPLICATION_JSON));
-
-        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-    }
 //    @Test
-//    public void testRegistrarAdmin() {
-//    	Admin admin = new Admin();
-//    	admin.setLogin(UUID.randomUUID().toString());
-//    	admin.setPassword("admin6");
-//    	
-//    	Response response = target.path("adminRegister")
-//                .request(MediaType.APPLICATION_JSON)
-//                .post(Entity.entity(admin, MediaType.APPLICATION_JSON));
+//    public void testRegisterUser() {
+//        User user = new User();
+//        user.setLogin(UUID.randomUUID().toString());
+//        user.setPassword("Sancet");
+//        user.setCorreo("Sancet@gmail.com");
+//
+//        Response response = target.path("register")
+//            .request(MediaType.APPLICATION_JSON)
+//            .post(Entity.entity(user, MediaType.APPLICATION_JSON));
 //
 //        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
 //    }
-//    
+    @Test
+    public void testRegistrarAdmin() {
+    	Admin admin = new Admin();
+    	admin.setLogin(UUID.randomUUID().toString());
+    	admin.setPassword("admin6");
+    	
+    	Response response = target.path("adminRegister")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(admin, MediaType.APPLICATION_JSON));
+
+        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
+    }
+    
 //    @Test
 //    public void testAddPelicula() {
 //    	Pelicula peli = new Pelicula();
